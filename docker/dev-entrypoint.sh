ff18f3e6473b7da2ba466daf8340907a786ef3bc
Ilya White <ilya.white@redhat.com>
2018-03-28 13:10:28 -0400

    Update docker container to be centos based

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -4,6 +4,6 @@ losetup -D
 losetup -P /dev/loop7 /dev/rh_disk
 losetup -P /dev/loop8 /dev/nrh_disk
 
-python cli.py -t ami-redhatami /dev/loop7 -t ami-centosami /dev/loop8
+scl enable rh-python36 'python cli.py -t ami-redhatami /dev/loop7 -t ami-centosami /dev/loop8'
 
 losetup -D
