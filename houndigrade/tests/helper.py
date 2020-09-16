390ad277c2eddccbe65f8529bb8ebe2b84f23f75
Brad Smith <brasmith@redhat.com>
2020-09-16 14:22:55 -0400

    refactor tests.helper to deduplicate file writing calls

--- houndigrade/tests/helper.py
+++ houndigrade/tests/helper.py
@@ -1,107 +1,96 @@
 """Helper functions for houndigrade tests."""
+import os
 import pathlib
-from textwrap import dedent
 
 from tests import data
 
 
-def prepare_fs(drive_path):
-    """Prepare a filesystem directory for testing."""
-    pathlib.Path("{}/xvdf1/etc/rhsm/syspurpose".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
-    pathlib.Path("{}/xvdf2/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
+def prepare_fs(root_path):
+    """
+    Prepare a filesystem directory for testing.
 
-    with open("{}/xvdf1/etc/redhat-release".format(drive_path), "w") as f:
-        f.write(data.REDHAT_RELEASE)
-    with open("{}/xvdf1/etc/os-release".format(drive_path), "w") as f:
-        f.write(data.OS_RELEASE_REDHAT)
-    with open(
-        "{}/xvdf1/etc/rhsm/syspurpose/syspurpose.json".format(drive_path), "w"
-    ) as f:
-        f.write(dedent(data.SYSPURPOSE_JSON_RHEL))
+    File tree under {root_path}/xvdf1 will look like RHEL.
+    File tree under {root_path}/xvdf2 will look like CentOS.
+    """
+    etc_path = f"{root_path}/xvdf1/etc"
+    write_data(data.OS_RELEASE_REDHAT, f"{etc_path}/os-release")
+    write_data(data.REDHAT_RELEASE, f"{etc_path}/redhat-release")
+    write_data(data.SYSPURPOSE_JSON_RHEL, f"{etc_path}/rhsm/syspurpose/syspurpose.json")
 
-    with open("{}/xvdf2/etc/centos-release".format(drive_path), "w") as f:
-        f.write(data.CENTOS_RELEASE)
-    with open("{}/xvdf2/etc/os-release".format(drive_path), "w") as f:
-        f.write(dedent(data.OS_RELEASE_CENTOS))
+    etc_path = f"{root_path}/xvdf2/etc"
+    write_data(data.OS_RELEASE_CENTOS, f"{etc_path}/os-release")
+    write_data(data.CENTOS_RELEASE, f"{etc_path}/centos-release")
 
 
-def prepare_fs_with_rhel_repos(drive_path):
-    """Prepare a filesystem directory for testing with enabled yum repos."""
-    pathlib.Path("{}/xvdf1/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    pathlib.Path("{}/xvdf2/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    pathlib.Path("{}/xvdf1/etc/yum.repos.d".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
-    pathlib.Path("{}/xvdf2/etc/yum.repos.d".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
+def prepare_fs_with_rhel_repos(root_path):
+    """
+    Prepare a filesystem directory for testing with enabled yum repos.
 
-    with open("{}/xvdf1/etc/yum.conf".format(drive_path), "w") as f:
-        f.write(data.YUM_CONF)
-    with open(
-        "{}/xvdf1/etc/yum.repos.d/rhel7-internal.repo".format(drive_path), "w"
-    ) as f:
-        f.write(data.YUM_REPO_RHEL_ENABLED)
-    with open("{}/xvdf1/etc/yum.repos.d/rhel.repo".format(drive_path), "w") as f:
-        f.write(data.YUM_REPO_RHEL_OPTIONAL_ENABLED)
-    with open("{}/xvdf2/etc/yum.conf".format(drive_path), "w") as f:
-        f.write(data.YUM_CONF)
-    with open(
-        "{}/xvdf2/etc/yum.repos.d/rhel7-internal.repo".format(drive_path), "w"
-    ) as f:
-        f.write(data.YUM_REPO_RHEL_ENABLED)
+    File tree under {root_path}/xvdf1 will have enabled RHEL yum repos.
+    File tree under {root_path}/xvdf2 will also have enabled RHEL yum repos.
+    """
+    etc_path = f"{root_path}/xvdf1/etc"
+    repos_path = f"{etc_path}/yum.repos.d"
+    write_data(data.YUM_CONF, f"{etc_path}/yum.conf")
+    write_data(data.YUM_REPO_RHEL_ENABLED, f"{repos_path}/rhel7-internal.repo")
+    write_data(data.YUM_REPO_RHEL_OPTIONAL_ENABLED, f"{repos_path}/rhel.repo")
+    etc_path = f"{root_path}/xvdf2/etc"
+    repos_path = f"{etc_path}/yum.repos.d"
+    write_data(data.YUM_CONF, f"{etc_path}/yum.conf")
+    write_data(data.YUM_REPO_RHEL_ENABLED, f"{repos_path}/rhel7-internal.repo")
 
 
-def prepare_fs_with_non_enabled_repos(drive_path):
-    """Prepare a filesystem directory for testing with disabled yum repos."""
-    pathlib.Path("{}/xvdf1/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    pathlib.Path("{}/xvdf2/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    pathlib.Path("{}/xvdf1/etc/yum.repos.d".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
-    pathlib.Path("{}/xvdf2/etc/yum.repos.d".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
+def prepare_fs_with_non_enabled_repos(root_path):
+    """
+    Prepare a filesystem directory for testing with disabled yum repos.
 
-    with open("{}/xvdf1/etc/yum.conf".format(drive_path), "w") as f:
-        f.write(data.YUM_CONF)
-    with open(
-        "{}/xvdf1/etc/yum.repos.d/rhel7-internal.repo".format(drive_path), "w"
-    ) as f:
-        f.write(data.YUM_REPO_RHEL_NOT_ENABLED)
-    with open("{}/xvdf2/etc/yum.conf".format(drive_path), "w") as f:
-        f.write(data.YUM_CONF)
-    with open("{}/xvdf2/etc/yum.repos.d/random.repo".format(drive_path), "w") as f:
-        f.write(data.YUM_REPO_RANDOM_ENABLED)
+    File tree under {root_path}/xvdf1 will have a not-enabled RHEL yum repo.
+    File tree under {root_path}/xvdf2 will also a not-enabled RHEL yum repo but also
+    an enabled not-RHEL repo.
+    """
+    etc_path = f"{root_path}/xvdf1/etc"
+    repos_path = f"{etc_path}/yum.repos.d"
+    write_data(data.YUM_CONF, f"{etc_path}/yum.conf")
+    write_data(data.YUM_REPO_RHEL_NOT_ENABLED, f"{repos_path}/rhel7-internal.repo")
+    etc_path = f"{root_path}/xvdf2/etc"
+    repos_path = f"{etc_path}/yum.repos.d"
+    write_data(data.YUM_CONF, f"{etc_path}/yum.conf")
+    write_data(data.YUM_REPO_RHEL_NOT_ENABLED, f"{repos_path}/rhel7-internal.repo")
+    write_data(data.YUM_REPO_RANDOM_ENABLED, f"{etc_path}/random.repo")
 
 
-def prepare_fs_with_reposdir_specified(drive_path):
-    """Prepare a filesystem directory for testing with custom yum repo dir."""
-    pathlib.Path("{}/xvdf1/etc".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    pathlib.Path("{}/xvdf1/etc/new_dir/yum_repos".format(drive_path)).mkdir(
-        parents=True, exist_ok=True
-    )
+def prepare_fs_with_reposdir_specified(root_path):
+    """
+    Prepare a filesystem directory for testing with custom yum repo dir.
 
-    with open("{}/xvdf1/etc/yum.conf".format(drive_path), "w") as f:
-        f.write(data.YUM_CONF_WITH_REPOSDIR)
-    with open(
-        "{}/xvdf1/etc/new_dir/yum_repos/rhel7-internal.repo".format(drive_path), "w"
-    ) as f:
-        f.write(data.YUM_REPO_RHEL_ENABLED)
+    File tree under {root_path}/xvdf1 will have a custom yum.conf that defines reposdir
+    at {root_path}/xvdf1/etc/new_dir/yum_repos which contains an enabled RHEL yum repo.
+    """
+    etc_path = f"{root_path}/xvdf1/etc"
+    repos_path = f"{etc_path}/new_dir/yum_repos"
+    write_data(data.YUM_CONF_WITH_REPOSDIR, f"{etc_path}/yum.conf")
+    write_data(data.YUM_REPO_RHEL_ENABLED, f"{repos_path}/rhel7-internal.repo")
 
 
-def prepare_fs_with_bad_yum_conf(drive_path):
+def prepare_fs_with_bad_yum_conf(root_path):
     """Prepare a filesystem directory for testing with a bad yum repo conf."""
-    pathlib.Path("{}/xvdf1/etc/".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    with open("{}/xvdf1/etc/yum.conf".format(drive_path), "wb") as f:
-        f.write(b"\xac")  # not a valid utf8 string!
+    yum_conf_path = "{}/xvdf1/etc/yum.conf".format(root_path)
+    write_non_utf8_data(yum_conf_path)
 
 
-def prepare_fs_with_bad_release_file(drive_path):
+def prepare_fs_with_bad_release_file(root_path):
     """Prepare a filesystem directory for testing with a bad release file."""
-    pathlib.Path("{}/xvdf1/etc/".format(drive_path)).mkdir(parents=True, exist_ok=True)
-    release_file_path = "{}/xvdf1/etc/potato-release".format(drive_path)
-    with open(release_file_path, "wb") as f:
-        f.write(b"\xac")  # not a valid utf8 string!
+    release_file_path = "{}/xvdf1/etc/potato-release".format(root_path)
+    write_non_utf8_data(release_file_path)
+
+
+def write_data(contents, file_path, mode="w"):
+    """Write contents to file_path, also ensuring its parent directory exists."""
+    pathlib.Path(os.path.dirname(file_path)).mkdir(parents=True, exist_ok=True)
+    with open(file_path, mode) as f:
+        f.write(contents)
+
+
+def write_non_utf8_data(file_path):
+    """Write non-utf8 "bad" data to file_path."""
+    write_data(b"\xac", file_path, "wb")  # not a valid utf8 string!
